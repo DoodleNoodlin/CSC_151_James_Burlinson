6ccb3ae3bd8346d25f0133229934a21e80d6b431
DoodleNoodlin <burlinsonj405@mail.sandhills.edu>
2026-02-01 17:33:17 -0500

    Mammal skeleton

---
+++ src/labs/example/mammal/Mammal.java
@@ -0,0 +1,38 @@
+package labs.example.mammal;
+
+public class Mammal {
+
+    private String hairColor;
+    private String eyeColor;
+    private double bodyTemp;
+    private double height;
+    private String breed;
+    private double weight;
+
+    public void run() {
+    }
+
+    public void eat() {
+    }
+
+    public void sleep() {
+    }
+
+    public void scratch() {
+    }
+
+    public void jump() {
+    }
+
+    public void growl() {
+    }
+
+    public void shake() {
+    }
+
+    public void bite() {
+    }
+
+    public void stretch() {
+    }
+}
